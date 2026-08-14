d04c797413ea6d07d9b166f09633edb2c089da9c
macdonutss <nafeesnazzim1@gmail.com>
2026-08-14 13:03:35 +0530

    Configure local database for development

--- config/settings.py
+++ config/settings.py
@@ -81,16 +81,28 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "recruitment_tracker",
-        "USER": "dbadmin",
-        "PASSWORD": "ANSY2026",
-        "HOST": "recruitment-tracker-db.cpik2u60ea8f.ap-southeast-1.rds.amazonaws.com",
-        "PORT": "5432",
+import os
+
+
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("DB_NAME"),
+            "USER": os.environ.get("DB_USER"),
+            "PASSWORD": os.environ.get("DB_PASSWORD"),
+            "HOST": os.environ.get("DB_HOST"),
+            "PORT": os.environ.get("DB_PORT", "5432"),
+        }
     }
-}
+
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "recruitment-tracker-cvs-844274146451")
